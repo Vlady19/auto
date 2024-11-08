4ba3c7e82c7c8fecb93fae0730bbbca4a828fc8e
bingbang <81105417+Vlady19@users.noreply.github.com>
2024-11-08 15:12:17 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 // Fonction pour convertir le solde en AI3
 function convertToAI3(balance) {
-  const conversionFactor = 1e18; // Facteur de conversion trouvé
+  const conversionFactor = 1e15; // Facteur de conversion trouvé
   return (balance / conversionFactor).toFixed(4); // Garde 4 décimales
 }
 
