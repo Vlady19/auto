a52e83a9800adb438ee91b98c46a4d7d57d50e0f
bingbang <81105417+Vlady19@users.noreply.github.com>
2024-11-14 00:27:14 +0100

    Update space-pledge.js

--- api/space-pledge.js
+++ api/space-pledge.js
@@ -4,10 +4,9 @@ import { activate } from '@autonomys/auto-utils';
 const MAINNET_WS_URL = 'wss://rpc.mainnet.subspace.foundation/ws';
 let api;
 
-// Fonction asynchrone pour activer l'API avec l'URL du Mainnet
 (async () => {
   try {
-    api = await activate(MAINNET_WS_URL);  // Activation avec l'URL Mainnet
+    api = await activate(MAINNET_WS_URL);
     console.log('API activated successfully on Mainnet.');
   } catch (error) {
     console.error('Error activating API on Mainnet:', error);
@@ -20,11 +19,10 @@ export default async function handler(req, res) {
       return res.status(503).json({ error: 'API not ready yet' });
     }
 
-    // Récupère la valeur `spacePledged` depuis la blockchain Mainnet
     const spacePledged = await spacePledge(api);
+    const blockHeight = await api.query.system.number(); // Récupère le block height
 
-    // Envoie la valeur de `spacePledged` sous forme de chaîne dans la réponse
-    res.status(200).json({ spacePledged: spacePledged.toString() });
+    res.status(200).json({ spacePledged: spacePledged.toString(), blockHeight: blockHeight.toString() });
   } catch (error) {
     console.error('Error fetching spacePledged on Mainnet:', error);
     res.status(500).json({ error: 'Failed to fetch space pledged' });
